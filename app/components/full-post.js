4fc3742d9dc11016269aa8a36d8de0e4c555e1fd
Gregory K and Midori Bowen <midori.p.bowen@gmail.com>
2016-02-24 14:28:29 -0800

    Add update post functionality.

--- app/components/full-post.js
+++ app/components/full-post.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  actions: {
+    update(post, params) {
+      this.sendAction('update', post, params);
+    },
+    delete(post) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('destroyPost', post);
+      }
+    }
+  }
 });
--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    delete(post) {
-      if (confirm('Are you sure you want to delete this post?')) {
-        this.sendAction('destroyPost', post);
-      }
-    }
-  }
+
 });
