315da300dd4c4df960510e91b4324fe8085865be
Gregory K and Midori Bowen <gregkanteres@gmail.com>
2016-02-25 11:48:59 -0800

    Refactor

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
+    return this.store.findAll('post').then(function(posts) {
+      return posts.sortBy('timestamp').reverse();
+    });
   },
-
-  actions: {
-    destroyPost(post) {
-      post.destroyRecord();
-      this.transitionTo('index');
-    }
-  }
 });
